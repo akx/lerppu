950c974fb1266d5f05af159dd007832b060b80dd
Aarni Koskela <akx@iki.fi>
2025-11-12 10:09:18 +0200

    Remove Dustin Home, it's done

--- lerppu/sources/sources.py
+++ lerppu/sources/sources.py
@@ -4,7 +4,6 @@ import httpx
 
 from lerppu.sources.base import ProductSource
 from lerppu.sources.datablocks import get_datablocks_sources
-from lerppu.sources.dustinhome import get_dustinhome_sources
 from lerppu.sources.jimms import get_jimms_sources
 from lerppu.sources.verk import get_verk_sources
 
@@ -13,5 +12,4 @@ def get_sources(sess: httpx.Client) -> Iterable[ProductSource]:
     yield from get_verk_sources(sess)
     yield from get_jimms_sources(sess)
     # yield from get_proshop_sources(sess)
-    yield from get_dustinhome_sources(sess)
     yield from get_datablocks_sources(sess)
